7bc9ce86cbfb049784ca2a3676b9ef5da512430e
Meelis Kurs <meelis.kurs@tktk.ee>
2022-10-12 00:11:57 +0300

    create new app

---
+++ MyFirstApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt tema nime  
+//rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName;
+
+
+userName = Console.ReadLine();  //input
+
+Console.WriteLine("Hello" + ", " + userName + "!");  //output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");  //output
+    
